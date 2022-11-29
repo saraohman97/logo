001cfbdf81098d13a85530e7d923ebb37ff964c2
saraohman97 <saraohman97@gmail.com>
2022-11-29 02:53:34 +0100

    first setup. Top navbar done

---
+++ src/components/TopNavbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TopNavbar = () => {
+  return (
+    <div className='top-nav'>
+        <small>Open: 9-15</small>
+        <small>Check out our latest SALE</small>
+        <small className='social-icons'>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-square-facebook"></i>
+            <i class="fa-brands fa-linkedin"></i>
+        </small>
+    </div>
+  )
+}
+
+export default TopNavbar
