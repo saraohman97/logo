5e5556cba5f868d4275acbb03cc06d467d8d4598
saraohman97 <saraohman97@gmail.com>
2022-11-29 20:08:13 +0100

    Added a navbar and three showcases on the main view.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import cart from '../imgs/cart.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+    <div className='navbar'>
+      <h1>LOGO</h1>
+      <div className="menu">
+        <p>Woman / Man</p>
+
+        <div className="nav-links">
+          <NavLink>Categories</NavLink>
+          <NavLink>Jewelry</NavLink>
+          <NavLink>Sale</NavLink>
+          <NavLink>Occassion</NavLink>
+        </div>
+
+        <div className="icons">
+          <p>Cart - $ 0</p>
+          <img src={cart} alt="" className='cart' />
+          <i className="fa-solid fa-heart"></i>
+        </div>
+      </div>
+    </div>
+
+    <div className="nav-border"></div>
+    </>
   )
 }
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import test from '../imgs/test.jpg'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+
+      <div className="showcase-1">
+        <h3>Look out for Autumns newest arrivals <i className="fa-solid fa-circle-arrow-right"></i></h3>
+        <div className="product-wrapper-1">
+          <img src={test} alt="" />
+          <img src={test} alt="" />
+          <img src={test} alt="" />
+
+          <i className="fa-solid fa-chevron-right"></i>
+        </div>
+      </div>
+
+      <div className="showcase-2">
+        <h2>Look out for Autumns newest arrivals</h2>
+        <div className="product-wrapper-2">
+          <img src={test} alt="" />
+          <img src={test} alt="" />
+          <img src={test} alt="" />
+        </div>
+        <i className="fa-solid fa-chevron-right"></i>
+      </div>
+
+      <div className="showcase-3">
+        <h3>Look out for Autumns </h3> 
+        <h2>newest arrivals </h2> 
+          <img src={test} alt="" />
+          <img src={test} alt="" />
+      </div>
+
+    </div>
   )
 }
 
